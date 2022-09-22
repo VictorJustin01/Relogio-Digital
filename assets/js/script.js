3cf131f099aed6ce63369e67e72c4bc0cdb1809f
João Victor <victor.developer@gmail.com>
2022-09-22 15:52:33 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const relogio = setInterval(function time(){
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let seg = dateToday.getSeconds();
+
+    if(hr < 10) hr = '0' + hr;
+    if(min < 10) min = '0' + min;
+    if(seg < 10) seg = '0' + seg;
+
+    hours.textContent = hr;
+    minutes.textContent = min;
+    seconds.textContent = seg;
+
+})
